8cf18e5febc887dc34b42899ee0bc94f3a0144ae
msundin <msundin.git@gmail.com>
2024-04-23 02:08:08 +0200

    Fixed keymaps.lua

--- linux-and-mac/.config/nvim/lua/custom/keymaps.lua
+++ linux-and-mac/.config/nvim/lua/custom/keymaps.lua
@@ -1,6 +1,3 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-local map = vim.keymap.asdfasdf
+local map = vim.keymap.set
 
-map('i', 'jk', '<esc>', { desc = 'Escape from insert mode' })
+-- map('i', 'jk', '<esc>', { desc = 'Escape from insert mode' })
