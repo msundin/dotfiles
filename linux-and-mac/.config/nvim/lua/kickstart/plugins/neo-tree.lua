2912006949eadfb1056606dce195ccba3b859c6c
msundin <msundin.git@gmail.com>
2024-05-03 21:04:22 +0200

    neo-tree: Focus on opened file

--- linux-and-mac/.config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ linux-and-mac/.config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -23,6 +23,11 @@ return {
           ['h'] = 'open',
         },
       },
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        --               -- the current file is changed while the tree is open.
+        leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
     },
   },
 }
