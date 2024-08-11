cd561ba5158c6148f9a15642d1e11341f465da2c
msundin <msundin.git@gmail.com>
2024-08-11 18:44:02 +0200

    installed flash plugin for jumping around

--- common/nvim/.config/nvim/lua/custom/plugins/init.lua
+++ common/nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -7,6 +7,26 @@ return {
 
   -- see debug lua config file
   -- { 'Mgenuit/nvim-dap-kotlin', config = function() end },
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {
+      label = {
+        -- uppercase = false,
+        -- ignore = 'qzxybgvjmk',
+      },
+    },
+
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
 
   {
     'max397574/better-escape.nvim',
