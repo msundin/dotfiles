497e873be1120e0c34bd127aa7dee65e6c6a2cc6
msundin <msundin.git@gmail.com>
2025-04-25 13:31:54 +0200

    copilot tab disable, ideavim and obsidian.nvim configs

--- common/nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/obsidian-nvim.lua
+++ common/nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/obsidian-nvim.lua
@@ -467,4 +467,28 @@ return {
       end,
     },
   },
+
+  -- {
+  --   "saghen/blink.cmp",
+  --   dependencies = { "saghen/blink.compat" },
+  --   opts = {
+  --     sources = {
+  --       default = { "obsidian", "obsidian_new", "obsidian_tags" },
+  --       providers = {
+  --         obsidian = {
+  --           name = "obsidian",
+  --           module = "blink.compat.source",
+  --         },
+  --         obsidian_new = {
+  --           name = "obsidian_new",
+  --           module = "blink.compat.source",
+  --         },
+  --         obsidian_tags = {
+  --           name = "obsidian_tags",
+  --           module = "blink.compat.source",
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 }
