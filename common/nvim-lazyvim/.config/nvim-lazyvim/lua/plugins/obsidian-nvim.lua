232d2e93872c2f851cd33080b701c41e01f62717
msundin <msundin.git@gmail.com>
2025-05-01 19:19:02 +0200

    Revert "* "sort_by" and "sort_reversed" work with nvim-cmp and telescope picker"
    This reverts commit ba20fb4e42843f12e52a8ef5c3379755c5e09e65.

--- common/nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/obsidian-nvim.lua
+++ common/nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/obsidian-nvim.lua
@@ -24,18 +24,18 @@ return {
     -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
     completion = {
       -- Enables completion using nvim_cmp
-      nvim_cmp = true,
+      nvim_cmp = false,
       -- Enables completion using blink.cmp
-      blink = false,
+      blink = true,
       -- Trigger completion at 2 chars.
       min_chars = 2,
     },
 
     picker = {
       -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', 'snacks.pick' or 'mini.pick'.
-      name = "telescope.nvim",
+      -- name = "telescope.nvim",
       -- name = "fzf-lua",
-      -- name = "snacks.pick",
+      name = "snacks.pick",
       -- Optional, configure key mappings for the picker. These are the defaults.
       -- Not all pickers support all mappings.
       mappings = {
