4848c8c948e505c2ea9523307e9266d503e30f11
msundin <msundin.git@gmail.com>
2025-08-23 19:38:58 +0200

    support left screen in portrait in Sway and transparent rofi gap

--- common/nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/colorsheme.lua
+++ common/nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/colorsheme.lua
@@ -2,10 +2,22 @@ return {
 
   { "rmehri01/onenord.nvim" },
 
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- {
+  --   "AlexvZyl/nordic.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     require("nordic").load()
+  --   end,
+  -- },
+
   {
     "LazyVim/LazyVim",
     opts = {
+      -- colorscheme = "catppuccin-mocha", -- onenord, catppuccin-mocha, catppuccin-macchiato, catppuccin-frappe, catppuccin-latte
       colorscheme = "onenord",
+      -- colorscheme = "nordic",
     },
   },
 }
