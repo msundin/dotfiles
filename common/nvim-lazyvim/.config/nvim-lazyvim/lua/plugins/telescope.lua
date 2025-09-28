f5bbcd520bb1e5848a94786868162885b4e82d9b
msundin <msundin.git@gmail.com>
2025-09-28 14:58:19 +0200

    fix telescope preview laptop screen Arch Linux

--- common/nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/telescope.lua
+++ common/nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/telescope.lua
@@ -3,14 +3,35 @@ return {
   opts = function(_, opts)
     return vim.tbl_deep_extend("force", opts or {}, {
       defaults = {
-        layout_strategy = "vertical",
+        layout_strategy = "flex",
         layout_config = {
+          flex = {
+            flip_columns = 120, -- Switch to horizontal layout on wider screens
+            flip_lines = 40, -- Switch based on height
+          },
           vertical = {
             preview_height = 0.7,
             prompt_position = "bottom",
-            mirror = false, -- Keep preview on top
+            mirror = false,
+            preview_cutoff = 10,
+            width = 0.9,
+            height = 0.9,
+          },
+          horizontal = {
+            preview_width = 0.6,
+            preview_cutoff = 10,
+            width = 0.9,
+            height = 0.9,
           },
         },
+        -- Alternative: force preview to always show
+        preview = {
+          hide_on_startup = false,
+        },
+        -- Ensure proper window sizing
+        winblend = 0,
+        wrap_results = true,
+        scroll_strategy = "limit",
       },
     })
   end,
