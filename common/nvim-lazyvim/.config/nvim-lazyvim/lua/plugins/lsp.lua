9e26538e944793541fd4c3af4fa9b4d39f1650d7
msundin <msundin.git@gmail.com>
2025-10-27 15:23:31 +0100

    Fixed depricated in lsp config

--- common/nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/lsp.lua
+++ common/nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/lsp.lua
@@ -1,11 +1,14 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- disable a keymap
-    keys[#keys + 1] = { "K", false } -- conflicts with `go buffer right` in Neovim with corne
-    -- add a keymap
-    keys[#keys + 1] = { "gh", vim.lsp.buf.hover, desc = "Goto Hover Documentation" }
-    keys[#keys + 1] = { "<leader>rn", vim.lsp.buf.rename, desc = "[R]e[n]ame" }
-  end,
+  opts = {
+    servers = {
+      ["*"] = {
+        keys = {
+          { "K", false }, -- conflicts with `go buffer right` in Neovim with corne
+          { "gh", vim.lsp.buf.hover, desc = "Goto Hover Documentation" },
+          { "<leader>rn", vim.lsp.buf.rename, desc = "[R]e[n]ame" },
+        },
+      },
+    },
+  },
 }
